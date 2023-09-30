2ea383d9fccacecab297fa2b1b01a2786d3bb32c
eib91 <nleib97@outlook.com>
2023-09-30 16:47:42 +0400

    Added support for multiple instruments and simplified code

--- pygenmus/main.py
+++ pygenmus/main.py
@@ -1,14 +1,22 @@
 import os
 import random
 import json
-import numpy as np
-import fluidsynth
 from midi2audio import FluidSynth as MidiFluidSynth
 from mido import MidiFile, MidiTrack, Message
 
-instruments = ["piano", "guitar", "violin", "drums"]
+instruments = ["piano", "guitar", "violin", "drums", "flute", "trumpet", "saxophone", "clarinet"]
 notes = ["C", "D", "E", "F", "G", "A", "B"]
-styles = ["fingerpicking", "strumming", "legato", "staccato"]
+
+instrument_programs = {
+    "piano": 0,
+    "guitar": 24,
+    "violin": 40,
+    "drums": 118,
+    "flute": 73,
+    "trumpet": 56,
+    "saxophone": 66,
+    "clarinet": 71,
+}
 
 root_dir = "Dataset Root"
 
@@ -31,43 +39,32 @@ for instrument in instruments:
         if not os.path.exists(note_dir):
             os.mkdir(note_dir)
 
-        for style in styles:
-            file_name = f"{instrument}_{note}_{style}.wav"
-            file_path = os.path.join(note_dir, file_name)
+        file_name = f"{instrument}_{note}.wav"
+        file_path = os.path.join(note_dir, file_name)
 
-            file_counter += 1
+        file_counter += 1
 
-            midi_file = MidiFile()
-            midi_track = MidiTrack()
-            midi_file.tracks.append(midi_track)
+        midi_file = MidiFile()
+        midi_track = MidiTrack()
+        midi_file.tracks.append(midi_track)
 
-            program = instruments.index(instrument) + 1
-            midi_track.append(Message("program_change", program=program))
+        program = instrument_programs.get(instrument, 0)
+        midi_track.append(Message("program_change", program=program))
 
-            note_value = notes.index(note) + 60
-            velocity = 100
-            duration = 1000
+        note_value = notes.index(note) + 60
+        velocity = 100
+        duration = 1000
 
-            if instrument == "guitar":
-                if style == "fingerpicking":
-                    channels = [1, 2, 3, 4, 5, 6]
-                    strings = [40, 45, 50, 55, 59, 64]
+        midi_track.append(Message("note_on", note=note_value, velocity=velocity))
+        midi_track.append(Message("note_off", note=note_value, time=duration))
 
-                    for channel, string in zip(channels, strings):
-                        midi_track.append(Message("program_change", channel=channel, program=program))
-                        note_value = string + notes.index(note)
-                        velocity = random.randint(64, 127)
-                        timing = random.randint(0, 500)
-                        midi_track.append(Message("note_on", channel=channel, note=note_value, velocity=velocity))
-                        midi_track.append(Message("note_off", channel=channel, note=note_value, time=duration + timing))
+        temp_midi_path = f"temp_{file_name}.mid"
+        midi_file.save(temp_midi_path)
 
-            temp_midi_path = f"temp_{file_name}.mid"
-            midi_file.save(temp_midi_path)
+        fs.midi_to_audio(temp_midi_path, file_path)
+        os.remove(temp_midi_path)
 
-            fs.midi_to_audio(temp_midi_path, file_path)
-            os.remove(temp_midi_path)
-
-            metadata[file_name] = {"instrument": instrument, "note": note, "style": style}
+        metadata[file_name] = {"instrument": instrument, "note": note}
 
 metadata_path = os.path.join(root_dir, "metadata.json")
 with open(metadata_path, "w") as f:
